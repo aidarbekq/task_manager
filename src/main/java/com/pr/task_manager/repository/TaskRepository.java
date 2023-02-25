34a2371c1a494d685dfe413a49eba2dc97818703
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-02-25 10:26:00 +0600

    modify

--- src/main/java/com/pr/task_manager/repository/TaskRepository.java
+++ src/main/java/com/pr/task_manager/repository/TaskRepository.java
@@ -1,11 +1,12 @@
 package com.pr.task_manager.repository;
 
 import com.pr.task_manager.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
-@Repository
 public interface TaskRepository extends CrudRepository<Task, Integer> {
 
+    Task findOne(int id);
 }
