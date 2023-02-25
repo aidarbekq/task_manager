0887597007611869dbc4453591404408d8e997bf
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-02-25 10:48:25 +0600

    add Main Controller

--- src/main/java/com/pr/task_manager/controller/MainController.java
+++ src/main/java/com/pr/task_manager/controller/MainController.java
@@ -1,4 +1,35 @@
 package com.pr.task_manager.controller;
 
+import com.pr.task_manager.service.TaskService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class MainController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/")
+    public String allTasks(HttpServletRequest request) {
+        request.setAttribute("tasks", taskService.findAll());
+        request.setAttribute("mode", "MODE_TASKS");
+        return "index";
+    }
+
+    @GetMapping("/new-task")
+    public String newTask(HttpServletRequest request) {
+        request.setAttribute("mode", "MODE_NEW");
+        return "index";
+    }
+
+    @GetMapping("/tasks/{id}")
+    public String updateTask(@PathVariable("id") int id, HttpServletRequest request) {
+        request.setAttribute("task", taskService.findTask(id));
+        request.setAttribute("mode", "MODE_UPDATE");
+        return "index";
+    }
 }
