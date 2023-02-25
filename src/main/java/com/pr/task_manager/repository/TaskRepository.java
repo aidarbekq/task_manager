7365cf7edc48f088166ef9038ed2d3dd2a96aeaf
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-02-25 09:55:53 +0600

    add repository for task

--- src/main/java/com/pr/task_manager/repository/TaskRepository.java
+++ src/main/java/com/pr/task_manager/repository/TaskRepository.java
@@ -1,5 +1,11 @@
 package com.pr.task_manager.repository;
 
-public interface TaskRepository {
+import com.pr.task_manager.model.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Integer> {
 
 }
